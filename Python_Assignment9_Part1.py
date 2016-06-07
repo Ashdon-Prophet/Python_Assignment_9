b31e32d49960be55c6c12a9f9178c2d3db79adbd
Ashdon-Prophet <ashdonprophet@gmail.com>
2016-06-06 20:22:03 -0400

    Names v.1
    Coding Dojo Python Assignment 9: Names

---
+++ Python_Assignment9_Part1.py
@@ -0,0 +1,2 @@
+# Coding Dojo Week 3: Python Fundamentals Assignment 9 - Names Part 1
+# Ashdon Prophet 06/06/16
---
+++ Python_Assignment9_Part2.py
@@ -0,0 +1,2 @@
+# Coding Dojo Week 3: Python Fundamentals Assignment 9 - Names Part 2
+# Ashdon Prophet 06/06/16
